f1a6f6d1b8b6e945a800236f4a9011e14bdb203b
Ekaraja <75461147+Ekaraja@users.noreply.github.com>
2022-11-27 23:51:39 +0100

    trying to fix deployment issues 16

--- src/components/StackCards.jsx
+++ src/components/StackCards.jsx
@@ -1,16 +1,19 @@
 import * as icon from "./icons/icons"
+// import { icons } from "./icons/icons";
 import "./styles/StackCards.css"
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/themes/light.css';
 
 
+
 const LanguageIcon = (props) => {
   return (
-    // <Tippy content={props.language} placement="bottom" theme="light">
-    //   <li>{props.icon}</li>
-    // </Tippy>
-    <li>{props.icon}</li>
+
+    <Tippy content={props.language} placement="bottom" theme="light">
+      <p>{props.icon}</p>
+    </Tippy>
+    // <li>{props.icon}</li>
   )
 }
 
@@ -24,10 +27,13 @@ function StackCards() {
         <LanguageIcon language="JavaScript" icon={icon.javascript} />
         <LanguageIcon language="React" icon={icon.react} />
         <LanguageIcon language="Ruby" icon={icon.ruby} /> */}
-        <LanguageIcon icon={icon.ruby} />
+        {/* <LanguageIcon icon={icon.ruby} />
         <Tippy content="CSS3">
           <p>HTML</p>
-        </Tippy>
+        </Tippy> */}
+
+         <LanguageIcon language="React" icon={icon.react} />
+         
       </ul>
     </div>
   );
