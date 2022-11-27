147ea9b19c22f6f8985f38a965e7f29933f79dde
Ekaraja <75461147+Ekaraja@users.noreply.github.com>
2022-11-27 23:04:07 +0100

    trying to fix deployment issues 11

--- src/components/StackCards.jsx
+++ src/components/StackCards.jsx
@@ -6,27 +6,28 @@ import 'tippy.js/themes/light.css';
 
 
 const LanguageIcon = (props) => {
-  <Tippy content={props.language} placement="bottom" theme="light">
-    <li>{props.icon}</li>
-  </Tippy>
-  }
+  return (
+    <Tippy content={props.language} placement="bottom" theme="light">
+      <li>{props.icon}</li>
+    </Tippy>
+  )
+}
 
 
 function StackCards() {
 
 
-    return ( 
-        <div>
-            <ul className="language-cards">
-                        <LanguageIcon language="HTML" icon={icon.html}/>
-                        <LanguageIcon language="CSS" icon={icon.css}/>
-                        <LanguageIcon language="JavaScript" icon={icon.javascript}/>
-                        <LanguageIcon language="React" icon={icon.react}/>
-                        <LanguageIcon language="Ruby" icon={icon.ruby}/> 
-                         {/* <li>{icon.javascript}</li> */}
-            </ul>
-        </div>
-     );
+  return (
+    <div>
+      <ul className="language-cards">
+        <LanguageIcon language="HTML" icon={icon.html} />
+        <LanguageIcon language="CSS" icon={icon.css} />
+        <LanguageIcon language="JavaScript" icon={icon.javascript} />
+        <LanguageIcon language="React" icon={icon.react} />
+        <LanguageIcon language="Ruby" icon={icon.ruby} />
+      </ul>
+    </div>
+  );
 }
 
 export default StackCards;
